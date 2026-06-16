f7f134cb1628923c1594f2dcd9e82de8e265d2ce
arturkonczalski <arturkonczalski@gmail.com>
2026-06-16 14:07:55 +0200

    poprawa skalowania

--- script.js
+++ script.js
@@ -1,13 +1,42 @@
-const mapa = document.getElementById("mapa");
+const imageWidth = 2560;
+const imageHeight = 1656;
+
+const map = L.map("map", {
+    crs: L.CRS.Simple,
+    minZoom: -2,
+    maxZoom: 3
+});
+
+const bounds = [
+    [0, 0],
+[imageHeight, imageWidth]
+];
+
+L.imageOverlay("assets/mapa.jpg", bounds).addTo(map);
+
+map.fitBounds(bounds);
+
 const coords = document.getElementById("coords");
 
-mapa.addEventListener("mousemove", (event) => {
-    const rect = mapa.getBoundingClientRect();
+map.on("mousemove", function (e) {
 
-    const x = (event.clientX - rect.left) / rect.width;
-    const y = (event.clientY - rect.top) / rect.height;
+    const x = e.latlng.lng / imageWidth;
+    const y = e.latlng.lat / imageHeight;
 
     coords.innerHTML =
     `X: ${x.toFixed(4)}<br>` +
     `Y: ${y.toFixed(4)}`;
+});
+
+map.on("contextmenu", function (e) {
+
+    const x = (e.latlng.lng / imageWidth).toFixed(4);
+    const y = (e.latlng.lat / imageHeight).toFixed(4);
+
+    navigator.clipboard.writeText(`${x},${y}`);
+
+    L.marker(e.latlng)
+    .addTo(map)
+    .bindPopup(`Skopiowano:<br>${x},${y}`)
+    .openPopup();
 });
