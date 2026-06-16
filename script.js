0f39f8c917e0ae4f7f8b3fe0b95d2a9eaea74d95
arturkonczalski <arturkonczalski@gmail.com>
2026-06-16 14:00:37 +0200

    poprawa style i script

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Mapa uruchomiona");
+const mapa = document.getElementById("mapa");
+const coords = document.getElementById("coords");
+
+mapa.addEventListener("mousemove", (event) => {
+    const rect = mapa.getBoundingClientRect();
+
+    const x = (event.clientX - rect.left) / rect.width;
+    const y = (event.clientY - rect.top) / rect.height;
+
+    coords.innerHTML =
+    `X: ${x.toFixed(4)}<br>` +
+    `Y: ${y.toFixed(4)}`;
+});
